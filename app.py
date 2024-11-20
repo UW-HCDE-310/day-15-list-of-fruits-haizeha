a2394f64b4c111297a4e2d228eb877a01c886d90
Hyeran Ha <haize@Hyerans-MacBook-Air.local>
2024-11-20 00:05:03 -0800

    updated day15

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import requests
+import keys
 
 app = Flask(__name__)
 @ app.route("/")
@@ -8,7 +10,25 @@ def index():
 		{"name":"oranges", "quantity":2},
 		{"name":"strawberries", "quantity":6}
 	]
-	return render_template("index.html", fruits = fruits)
+
+	filtered_fruits = [
+		fruit for fruit in fruits
+		if fruit["name"].startswith("o") and fruit["quantity"] < 3
+	]
+
+	print(keys.MY_SECRET_API_KEY_1)
+	print(keys.MY_SECRET_API_KEY_2)
+
+	response = requests.get("https://api.sunrise-sunset.org/json?lat=47.6062&lng=-122.3321&formatted=0")
+	if response.status_code == 200:
+		data = response.json()
+		sunrise = data["results"]["sunrise"]
+		sunset = data["results"]["sunset"]
+	else:
+		sunrise = "N/A"
+		sunset = "N/A"
+
+	return render_template("index.html", fruits = filtered_fruits, sunrise=sunrise, sunset=sunset)
 
 if __name__ == '__main__':
 	app.run(debug=True)
